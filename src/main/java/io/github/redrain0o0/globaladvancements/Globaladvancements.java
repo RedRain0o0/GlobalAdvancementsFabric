eaf5a5b66cb06188cd1d306860cf33d1fa100d43
Ori <redraino.o.o.o.o@gmail.com>
2026-02-08 15:33:32 -0500

    initial commit

---
+++ src/main/java/io/github/redrain0o0/globaladvancements/Globaladvancements.java
@@ -0,0 +1,10 @@
+package io.github.redrain0o0.globaladvancements;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Globaladvancements implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/github/redrain0o0/globaladvancements/client/GlobaladvancementsClient.java
@@ -0,0 +1,10 @@
+package io.github.redrain0o0.globaladvancements.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlobaladvancementsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
