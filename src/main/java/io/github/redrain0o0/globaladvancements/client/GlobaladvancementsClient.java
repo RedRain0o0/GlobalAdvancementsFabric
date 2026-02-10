4b5877399a1eb4f1e4de8e5174dd49b5332dc3d9
Ori <redraino.o.o.o.o@gmail.com>
2026-02-10 08:40:50 -0500

    populate new advancements file with default contents

--- src/main/java/io/github/redrain0o0/globaladvancements/client/GlobaladvancementsClient.java
+++ src/main/java/io/github/redrain0o0/globaladvancements/client/GlobaladvancementsClient.java
@@ -1,32 +1,42 @@
 package io.github.redrain0o0.globaladvancements.client;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import io.github.redrain0o0.globaladvancements.Globaladvancements;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 public class GlobaladvancementsClient implements ClientModInitializer {
     private static File advancements;
+    private static Gson gson;
 
     @Override
     public void onInitializeClient() {
+        gson = new Gson();
+
+        // Create new Advancements file if it doesnt exist
         try {
-            Globaladvancements.LOGGER.info("{}", FabricLoader.getInstance().getGameDir());
             advancements = new File(FabricLoader.getInstance().getGameDir() + "/advancements.json");
             if (!advancements.exists()) {
                 Globaladvancements.LOGGER.info("Advancements file not found, creating new one");
-                Globaladvancements.LOGGER.info("{}", advancements.exists());
                 advancements.createNewFile();
-                Globaladvancements.LOGGER.info("{}", advancements.exists());
+                try (Writer writer = new FileWriter(advancements)) {
+                    String defaultFileContents = "{\"unlocked_advancements\":[]}";
+                    writer.write(defaultFileContents);
+                }
             }
         } catch (IOException e) {
             Globaladvancements.LOGGER.error("Error handling advancements file: " + e);
         }
     }
 
-    public static File getAdvancements() {
-        return advancements;
-    }
+    //public static JsonObject getAdvancements() {
+    //    gson.
+    //    return
+    //}
 }
